a9d01be93d4b445044db302473b501d966fc1f33
JoshuaMaddy <110144926+JoshuaMaddy@users.noreply.github.com>
2025-03-26 13:35:59 -0400

    Remove logging from alembic env.py to prevent overriding existing logging.

--- src/leaflock/alembic/env.py
+++ src/leaflock/alembic/env.py
@@ -1,5 +1,4 @@
 import os
-from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import create_engine, pool
@@ -10,10 +9,6 @@ from leaflock.sqlalchemy_tables import Base
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
